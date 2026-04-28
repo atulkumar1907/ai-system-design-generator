6a004fa9cb60da9c09b1f47c6d1604e3ece1941a
Atul Kumar <atulkashyap1907@gmail.com>
2026-04-28 23:10:44 +0530

    add routes and variables in index.css

--- architectai-client/src/App.tsx
+++ architectai-client/src/App.tsx
@@ -1,11 +1,30 @@
-import Home from "./features/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./features/home/Home";
+import { Navbar } from "./components/shared/navbar/Navbar";
+import Editor from "./features/Editor/Editor";
+import Compare from "./features/compare/Compare";
+import { Code, Save } from "lucide-react";
 
 
 export default function App() {
   return (
-    <div className=".dark">
-      <Home />
+    <div className="dark">
+      <BrowserRouter>
+        <Navbar />
+
+        {/* Add padding-top because navbar is fixed */}
+        <div className="pt-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/compare" element={<Compare />} />
+            <Route path="/code" element={<Code />} />
+            <Route path="/saved" element={<Save />} />
+          </Routes>
+        </div>
+
+      </BrowserRouter>
     </div>
-  )
+  );
 }
